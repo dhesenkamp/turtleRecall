9438111389675b051dec1e1e722bf2690655aff8
Dennis Hesenkamp <dhesenkamp@uos.de>
2022-04-07 15:49:22 +0200

    Update to sequential API

--- models/alexNet.py
+++ models/alexNet.py
@@ -1,56 +1,53 @@
-from tensorflow.keras import Model
+from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, MaxPool2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.metrics import CategoricalAccuracy, Precision
+
 
 # https://towardsdatascience.com/implementing-alexnet-cnn-architecture-using-tensorflow-2-0-and-keras-2113e090ad98
 # https://proceedings.neurips.cc/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
 
 
-class AlexNet(Model):
+def create_alexnet():
     """
     AlexNet convolutional neural network as described in Krizhevsky et al., 2012.
-    Works with input of shape (227, 227, 3).
+    Usually works with input of shape (227, 227, 3).
     """
 
-    def __init__(self):
-        super(AlexNet, self).__init__()
+    alexNet = Sequential([
+        Input(shape=(224,224,3)),
 
-        self.input_layer = Input(shape=(224,224,3))
-        self.all_layers = [
-            Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu'),
-            BatchNormalization(),
-            MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu'),
+        BatchNormalization(),
+        MaxPool2D(pool_size=(3,3), strides=(2,2)),
 
-            Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
-            BatchNormalization(),
-            MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
+        BatchNormalization(),
+        MaxPool2D(pool_size=(3,3), strides=(2,2)),
 
-            Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-            BatchNormalization(),
-            Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-            BatchNormalization(),
+        Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        BatchNormalization(),
+        Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        BatchNormalization(),
 
-            Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
-            BatchNormalization(),
-            MaxPool2D(pool_size=(3,3), strides=(2,2)),
+        Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+        BatchNormalization(),
+        MaxPool2D(pool_size=(3,3), strides=(2,2)),
 
-            Flatten(),
-            Dense(units=4096, activation='relu'),
-            Dropout(0.5),
-            Dense(units=4096, activation='relu'),
-            Dropout(rate=0.5),
-            Dense(units=254, activation='softmax')
-        ]
-        self.out = self.call(self.input_layer)
-        
-    
-    def call(self, x, training=False):
-        for layer in self.all_layers:
-            try:
-                x = layer(x, training=training)
-            except:
-                x = layer(x)
-        return x
+        Flatten(),
+        Dense(units=4096, activation='relu'),
+        Dropout(0.5),
+        Dense(units=4096, activation='relu'),
+        Dropout(rate=0.5),
 
+        Dense(units=254, activation='softmax')
+    ])
 
-def create_alexnet():
-    pass
+    alexNet.compile(
+        optimizer=Adam(),
+        loss=CategoricalCrossentropy(),
+        metrics=[CategoricalAccuracy(), Precision(top_k=5)]
+)
+
+    return alexNet
