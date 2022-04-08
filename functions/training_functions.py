373fb3f705ee6ce6c017b69003ec87fd6c6633b2
Dennis Hesenkamp <dhesenkamp@uos.de>
2022-04-08 19:30:48 +0200

    Docstrings + removal of unnecessary imports

--- functions/training_functions.py
+++ functions/training_functions.py
@@ -1,12 +1,21 @@
 import tensorflow as tf
-import numpy as np
-import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from tqdm import tqdm, trange
 
 
 def train_step(model, input, target, loss_function, optimizer):
+    """
+    Utility function to use with training of sub-classed models. Trains model on given data.
+
+    Args:
+        model (tf.keras.Model): model instance to train
+        input: training data
+        target: to training data corresponding labels
+        loss_function (tf.keras.losses object): loss function to calculate loss of the model
+        optimizer (tf.keras.optimizer object): optimizer to use for training the model
+    Returns:
+        loss (float): combined loss of the model after training on the given data
+    """
     with tf.GradientTape() as tape:
         prediction = model(input, training=True)
         loss = loss_function(target, prediction)
@@ -18,6 +27,17 @@ def train_step(model, input, target, loss_function, optimizer):
 
 
 def test_step(model, test_data, loss_function):
+    """
+    Utility function to use with training of sub-classed models. Performs model evaluation on test data.
+
+    Args: 
+        model (tf.keras.Model): model instance to test
+        test_data (tf.data.Dataset): dataset for testing
+        loss_function (tf.keras.losses object): loss function to calculate loss of the model
+    Returns:
+        test_loss (float): mean loss of the model on the test set
+    """
+
     #test_accuracy_aggregator = []
     test_loss_aggregator = []
 
@@ -33,6 +53,21 @@ def test_step(model, test_data, loss_function):
 
 
 def training_loop(train_data, test_data, model, loss_function, optimizer, nr_epochs, plot=True):
+    """
+    Utility function to train sub-classed models. Loops nr_epochs times over the given dataset.
+
+    Args:
+        train_data (tf.data.Dataset): dataset for training
+        test_data (tf.data.Dataset): dataset for testing
+        model (tf.keras.Model): model instance to train
+        loss_function (tf.keras.losses object): loss function to use for backpropagation        
+        optimizer (tf.keras.optimizer object): optimizer to use for training the model
+        nr_epochs (int): number of epochs to train for
+        plot (Bool): whether to visualize results of the training process in a graph. Default: True
+    Returns:
+        None
+    """
+
     tf.keras.backend.clear_session()
 
     train_losses = []
@@ -55,6 +90,7 @@ def training_loop(train_data, test_data, model, loss_function, optimizer, nr_epo
         test_loss = test_step(model, test_data, loss_function)
         test_losses.append(test_loss)
     
+    # return plot with training metrics if desired
     if plot:
         plt.figure()
         plt.plot(train_losses, label='train loss')
