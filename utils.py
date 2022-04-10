4d4960a1ce1253a38fc0fcdc6339e13a4ea1156e
Dennis Hesenkamp <dhesenkamp@uos.de>
2022-04-10 20:39:21 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,7 @@
-import tensorflow as tf
-import numpy as np
-import pandas as pd
-import seaborn as sns
-
-
 IMAGE_DIR = './data/images'
 MIN_NR_IMGS = 10
 IMAGE_SIZE = (224,224)
 INPUT_SHAPE = IMAGE_SIZE + (3,)
-NUM_CLASSES = 254
-BATCH_SIZE = 128
-NR_EPOCHS = 50
+BATCH_SIZE = 64
+NR_EPOCHS = 10
 
